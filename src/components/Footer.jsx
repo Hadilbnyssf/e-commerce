26a9fced099e8cffb0959d4a328e835c00368aae
Hadilbnyssf <bnyssfhadil@gmail.com>
2024-10-27 20:28:02 +0100

    added github icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,9 +7,11 @@ const Footer = () => {
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
             <p className="mb-3 mb-md-0">Made with ❤️ by {" "}
-              <a  href="" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">free template </a>
+              <a  href="" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">free template</a>
             </p>
-
+            <a className="text-dark fs-4" href="https://github.com/Hadilbnyssf/e-commerce" target="_blank" rel="noreferrer">
+              <i className="fa fa-github"></i>
+            </a>
           </div>
         </div>
       </footer>
